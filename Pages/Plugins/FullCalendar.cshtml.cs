fdc7b8592eaf3f5a643ccc1bc58f46f2f03b9f3c
NEIL R. MACABULOS <NEIL2003>
2023-01-24 14:43:41 +0800

    FINALS ACTIVITY

---
+++ Pages/Plugins/FullCalendar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NeilMacabulos.Midterm_.Pages.Plugins
+{
+    public class FullCalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
