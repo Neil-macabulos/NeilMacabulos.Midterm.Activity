58eca8f98e1ffd5ab4fc845ff50cbd8ff9d4b276
NEIL R. MACABULOS <NEIL2003>
2023-01-26 11:36:11 +0800

    Finals Exam

--- Pages/Manage/Products/Index.cshtml.cs
+++ Pages/Manage/Products/Index.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace NeilMacabulos.Midterm_.Pages.Manage.Users
+namespace NeilMacabulos.Midterm_.Pages.Manage.Products
 {
     public class Index : PageModel
     {
@@ -21,7 +21,7 @@ namespace NeilMacabulos.Midterm_.Pages.Manage.Users
             View = View ?? new ViewModel();
         }
 
-        public void OnGet(int? pageIndex = 1, int? pageSize = 10, string? sortBy = "", SortOrder sortOrder = SortOrder.Ascending, string? keyword = "", Guid? roleId = null)
+        public void OnGet(int? pageIndex = 1, int? pageSize = 10, string? sortBy = "", SortOrder sortOrder = SortOrder.Consumable, string? keyword = "", Guid? roleId = null)
         {
             var skip = (int)((pageIndex - 1) * pageSize);
 
@@ -37,80 +37,31 @@ namespace NeilMacabulos.Midterm_.Pages.Manage.Users
                 );
             }
 
-            if (categoriesId != null)
-            {
-                query = query.Where(a => a.CategoriesId == categoriesId);
-            }
-
             var totalRows = query.Count();
 
             if (!string.IsNullOrEmpty(sortBy))
             {
-                if (sortBy.ToLower() == "name" && sortOrder == SortOrder.Ascending)
+                if (sortBy.ToLower() == "name" && sortOrder == SortOrder.Consumable)
                 {
                     query = query.OrderBy(a => a.Name);
                 }
-                else if (sortBy.ToLower() == "name" && sortOrder == SortOrder.Descending)
+                else if (sortBy.ToLower() == "name" && sortOrder == SortOrder.NonConsumable)
                 {
                     query = query.OrderByDescending(a => a.Name);
                 }
-                else if (sortBy.ToLower() == "emailaddress" && sortOrder == SortOrder.Ascending)
+                else if (sortBy.ToLower() == "emailaddress" && sortOrder == SortOrder.Consumable)
                 {
                     query = query.OrderBy(a => a.EmailAddress);
                 }
-                else if (sortBy.ToLower() == "emailaddress" && sortOrder == SortOrder.Descending)
+                else if (sortBy.ToLower() == "emailaddress" && sortOrder == SortOrder.NonConsumable)
                 {
                     query = query.OrderByDescending(a => a.EmailAddress);
                 }
             }
 
-            var users = query
-                            .Skip(skip)
-                            .Take((int)pageSize)
-                            .ToList();
-
-            if (roleId != null)
-            {
-                View.RoleId = roleId;
-                View.RoleName = users.FirstOrDefault()?.Role?.Name;
-            }
-
-            View.Users = new Paged<Poducts>()
-            {
-                Items = categories,
-                PageIndex = pageIndex,
-                PageSize = pageSize,
-                TotalRows = totalRows,
-                SortBy = sortBy,
-                SortOrder = sortOrder,
-                Keyword = keyword,
-            };
-        }
-
-        public JsonResult? OnGetRolesLookup(int pageIndex = 1, string? keyword = "", int pageSize = 10)
-        {
-
-            var query = _context.Products.AsQueryable();
-
-            if (!string.IsNullOrEmpty(keyword))
-            {
-                query = query.Where(a =>
-                            a.Name != null && a.Name.ToLower().StartsWith(keyword.ToLower())
-                );
-            }
-
-            return new JsonResult(query?.Select(a => new LookupDto.Result()
-            {
-                Id = a.Id.ToString(),
-                Text = a.Name
-            })
-            .OrderBy(a => a.Text)
-            .GetLookupPaged(pageIndex, pageSize));
-        }
-
         public class ViewModel
         {
-            public Paged<Categories>? Categories { get; set; }
+            public Paged<Products>? Products { get; set; }
             public Guid? ProductsId { get; set; }
             public string? ProductsName { get; set; }
         }
